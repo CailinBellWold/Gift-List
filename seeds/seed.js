c564223da90813e66f1cf06ac2f11528fbf276c2
Cailin Bell Wold <79965490+CailinBellWold@users.noreply.github.com>
2021-06-29 22:15:15 -0700

    Working on handlebars.

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,10 +2,12 @@ const sequelize = require('../config/connection');
 const { User, Gifts, Recipient } = require('../models');
 
 // TODO: Make sure these path names match what are supplied by others.
-const userData = require('./user.json');
+const userData = require('./user-seed.json');
 const giftData = require('./gifts-seed.json');
-const recipientData = require('./recipient.json')
+// const recipientData = require('./recipient.json')
 
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
   // We had to pass individualHooks: true here because
   // we need to use bcrypt each userData.password ...
   await User.bulkCreate(userData, {
@@ -14,10 +16,11 @@ const recipientData = require('./recipient.json')
   });
 
   // Assuming we don't need to do anything extra as we create each recipient ...
-  await Recipient.bulkCreate(recipientData);
+  // await Recipient.bulkCreate(recipientData);
   await Gifts.bulkCreate(giftData);
 
   process.exit(0);
+
 };
 
 seedDatabase();
