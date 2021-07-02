fe8b248cc17a7bdf3307bd497963d58714bb5994
Danny Roubin <dannyroubin1@gmail.com>
2021-07-02 13:17:25 -0700

    created an update gift js file to handle the api request

---
+++ public/js/updategift.js
@@ -0,0 +1,24 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const giftee = document.querySelector("#giftGiftee").value.trim();
+  const description = document.querySelector("#giftDescription").value.trim();
+  const budget = document.querySelector("#giftBudget").value.trim();
+  const notes = document.querySelector("#giftNotes").value.trim();
+
+  if (giftee && description && budget && notes) {
+    const response = await fetch(`/api/giftRoutes`, {
+      method: "PUT",
+      body: JSON.stringify({ giftee, description, budget, notes }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace("/userlanding");
+    } else {
+      alert("Failed to update project");
+    }
+  }
+};
