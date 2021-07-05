5e02d9546efcea55807dd0e4ece8709255ad2429
Danny Roubin <dannyroubin1@gmail.com>
2021-07-05 15:32:34 -0700

    part of code is not working, but will continue to keep working on it

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,69 +1,69 @@
-const router = require('express').Router();
-const { Gifts: Gift, User } = require('../models');
-const { update } = require('../models/User');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { Gifts: Gift, User } = require("../models");
+const { update } = require("../models/User");
+const withAuth = require("../utils/auth");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    res.render('homepage', { 
-      logged_in: req.session.logged_in 
+    res.render("homepage", {
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/userlanding');
+    res.redirect("/userlanding");
     return;
   }
-  res.render('login');
+  res.render("login");
 });
 
 // Use withAuth middleware to prevent access to route
-router.get('/userlanding', withAuth, async (req, res) => {
+router.get("/userlanding", withAuth, async (req, res) => {
   try {
     const giftData = await Gift.findAll({
       include: [
         {
           model: User,
-          attributes: ['id'], // There's no 'name' attribute, correct?
+          attributes: ["id"], // There's no 'name' attribute, correct?
         },
       ],
     });
     // Serialize data so the template can read it
     const gifts = giftData.map((gift) => gift.get({ plain: true }));
-    
-    // Pass serialized data and session flag into template 
-    res.render('userlanding', { 
-      gifts, 
-      logged_in: req.session.logged_in 
+
+    // Pass serialized data and session flag into template
+    res.render("userlanding", {
+      gifts,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/newGift', withAuth, async (req, res) => {
-  res.render('newGift');
+router.get("/newGift", withAuth, async (req, res) => {
+  res.render("newGift");
 });
 
-router.get('/updateGift/:id', async (req, res) => {
+router.get("/updateGift/:id", withAuth, async (req, res) => {
   try {
     const giftData = await Gift.findByPk(req.params.id, {
       include: [
         {
           model: User,
-          attributes: ['email'],
+          attributes: ["email"],
         },
       ],
     });
     const gift = giftData.get({ plain: true });
-    res.render('updateGift', {
+    res.render("updateGift", {
       gift,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -71,7 +71,7 @@ router.get('/updateGift/:id', async (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
-router.get('/userlanding', withAuth, async (req, res) => {
+router.get("/userlanding", withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
@@ -81,33 +81,33 @@ router.get('/userlanding', withAuth, async (req, res) => {
 
     const user = userData.get({ plain: true });
 
-    res.render('userlanding', {
+    res.render("userlanding", {
       ...user,
-      logged_in: true
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/userlanding');
+    res.redirect("/userlanding");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
-  
+
 module.exports = router;
 
 // Use withAuth middleware to prevent access to route
 // Wasn't working on Donna's machine without this. We will take a look at this to see if we need it.
-router.get('/userlanding', withAuth, async (req, res) => {
-  console.log('get /userlanding called');
-  console.log('req.params:\n', JSON.stringify(req.params, null, 2));
-  console.log('req.session:\n', JSON.stringify(req.session, null, 2));
+router.get("/userlanding", withAuth, async (req, res) => {
+  console.log("get /userlanding called");
+  console.log("req.params:\n", JSON.stringify(req.params, null, 2));
+  console.log("req.session:\n", JSON.stringify(req.session, null, 2));
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
@@ -115,19 +115,19 @@ router.get('/userlanding', withAuth, async (req, res) => {
       // include: [{ model: User }], ... that will come by default.
     });
     const user = userData.get({ plain: true });
-    res.render('userlanding', {
+    res.render("userlanding", {
       ...user,
-      logged_in: true
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/userlanding');
+    res.redirect("/userlanding");
     return;
   }
-  res.render('login');
+  res.render("login");
 });
--- public/js/updategift.js
+++ public/js/updategift.js
@@ -1,26 +1,42 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
+  console.log("first stop");
   //DANNY TO-DO: Update/clean ID here and on newGift handlebars
   const recipientName = document.querySelector("#giftGiftee").value.trim();
   const description = document.querySelector("#giftDescription").value.trim();
   //   keep these two just in case we decide to include the budget and notes
   //   const budget = document.querySelector("#giftBudget").value.trim();
   //   const notes = document.querySelector("#giftNotes").value.trim();
+  console.log("second stop");
 
   if (recipientName && description) {
-    const response = await fetch(`/api/giftRoutes`, {
+    console.log("Third stop");
+    const response = await fetch(`/api/gifts`, {
       method: "PUT",
       body: JSON.stringify({ recipientName, description }),
       headers: {
         "Content-Type": "application/json",
       },
     });
+    console.log("fourth stop");
 
     if (response.ok) {
+      console.log("fifth stop");
       document.location.replace("/userlanding");
     } else {
       alert("Failed to update gift");
     }
+  }
+
+  const response = await fetch(`/api/gifts/${id}`, {
+    method: "PUT",
+  });
+
+  if (response.ok) {
+    console.log("final stop");
+    document.location.replace("/userlanding");
+  } else {
+    alert("Failed to delete project");
   }
 };
 
